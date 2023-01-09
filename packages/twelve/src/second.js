79f81395d5e541be2ae945df2e18dbdcb5051d69
feigebaobei <18515195415@163.com>
2023-01-09 12:02:35 +0800

    docs(vue): 使用新v-if

--- packages/twelve/src/second.js
+++
@@ -1,9 +0,0 @@
-let cd = require('@vue/compiler-dom')
-// let app = require('./app.vue')
-let clog = console.log
-
-// let template = '<div v-if="true">str</div>'
-let template = '<div v-if="visible">str</div><script setup>let visible = false</script>'
-let res = cd.compile(template)
-
-clog(res.code)
